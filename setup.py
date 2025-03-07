73e2e273d54c92a7b42c96ea35bb240710378cad
Nem0oo <gcourtot@gmail.com>
2025-03-07 00:10:15 +0000

    Make package instalable

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Insta-OSINT",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[
+        'requests',  # Ajoute toutes les dépendances nécessaires ici
+        'pathlib',   # pathlib est inclus dans Python >= 3.4, donc pas nécessaire si tu utilises cette version ou supérieure
+    ],
+    entry_points={
+        "console_scripts": [
+            "insta-osint = osint.main:main",
+        ],
+    },
+)
