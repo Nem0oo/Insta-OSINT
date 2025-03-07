083307a96238b6c0a9380cdff162c2357237bfc3
Nem0oo <gcourtot@gmail.com>
2025-03-07 04:03:40 +0000

    Making package more clear

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="Insta-OSINT",
     version="1.0.0",
-    packages=find_packages(),
+    packages=['osint'],
     install_requires=[
         'requests',  # Ajoute toutes les dépendances nécessaires ici
         'pathlib',   # pathlib est inclus dans Python >= 3.4, donc pas nécessaire si tu utilises cette version ou supérieure
